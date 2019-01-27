2519c15b2aa0b57ea2bf7b2788f4a78cc217c7f5
huitian <tanhuitian@gmail.com>
2019-01-28 01:19:33 +0800

    Created menu.html, TODO: Menu sizing

--- project0/main.js
+++ project0/main.js
@@ -1,20 +1,20 @@
 function validateForm() {
     if (isEmpty(document.getElementById('data_3').value.trim())) {
-    alert('Name is required!');
-    return false;
+        alert('Name is required!');
+        return false;
     }
     if (isEmpty(document.getElementById('data_6').value.trim())) {
-    alert('Date is required!');
-    return false;
+        alert('Date is required!');
+        return false;
     }
     if (isEmpty(document.getElementById('data_7').value.trim())) {
-    alert('Time is required!');
-    return false;
+        alert('Time is required!');
+        return false;
     }
     return true;
-    }
-    function isEmpty(str) { return (str.length === 0 || !str.trim()); }
-    function validateEmail(email) {
+}
+function isEmpty(str) { return (str.length === 0 || !str.trim()); }
+function validateEmail(email) {
     var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,15}(?:\.[a-z]{2})?)$/i;
     return isEmpty(email) || re.test(email);
-    }
+}
